f6a8147ee761a70e3188902dff88dadb1073ccf1
Md-Danish-eng <85988020+Md-Danish-eng@users.noreply.github.com>
2022-02-14 13:26:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Danish i am nodejs bro');
+  res.send('Hello Danish i am nodejs');
 });
 
 app.listen(PORT, HOST);
