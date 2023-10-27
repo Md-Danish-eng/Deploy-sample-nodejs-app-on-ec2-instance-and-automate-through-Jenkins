3c91e6eae3cff9bccafef50d06afed381857c17d
Md Danish <85988020+Md-Danish-eng@users.noreply.github.com>
2023-10-27 13:22:29 +0530

    Update and rename test.sh to testt.sh

---
+++ testt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source_dir="/home/ubuntu/test"
+cd $source_dir
+git clone https://github.com/Md-Danish-eng/Deploy-sample-nodejs-app-on-ec2-instance-and-automate-through-Jenkins.git github-action
+cd github-action
+sudo docker build -t action .
+sudo docker rm -f bloon 
+sudo docker run -dit --name bloon -p 9080:9080 action
